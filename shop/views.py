49ff8f7393abca451f85a96ae95cb894f2118f1c
shreyash <shreyash.hambarde@click2cloud.net>
2022-06-03 16:02:41 +0530

    Added the entries and displayed it

--- shop/views.py
+++ shop/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .models import item
 from shop.forms import ItemForm
 
 # Create your views here.
 def item_list(request):
-    return render(request, 'shop/item_list.html')
+    context = {'item_list': item.objects.all()}
+    return render(request, 'shop/item_list.html',context)
 
 # GET AND POST request (insert or update operations)
 def item_form(request):
-    form = ItemForm()
-    return render(request, 'shop/item_form.html',{'form':form})
-
+    if request.method == 'GET':
+        # GET request
+        form = ItemForm()
+        return render(request, 'shop/item_form.html', {'form': form})
+    else:
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            # POST request
+            form.save()
+        return redirect('/shop/list')
+   
 #  DELETE request (delete operations)
 def item_delete(request):
     return render(request, '')
